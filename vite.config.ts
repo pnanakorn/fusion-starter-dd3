7cb7aa434014cd3370d5a240dd24cd15fef6e748
pnanakorn <pnanakorn@connect.ust.hk>
2026-05-03 17:36:05 +0800

    Remove unused three.js deps, raise chunkSizeWarningLimit to 1000

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     outDir: "dist/spa",
+    chunkSizeWarningLimit: 1000,
   },
   plugins: [react(), expressPlugin()],
   resolve: {
